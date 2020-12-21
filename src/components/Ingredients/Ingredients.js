cf8ed61fcadda0c400d4cea8e46ca950fdfccdee
Paul Caruana <paul.caruana@leighton.com>
2020-12-21 17:04:23 +0000

    React hooks initial commit

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]);
+
+useEffect(() => {
+console.log('RENDERING INGREDIENTS', userIngredients);
+}, [userIngredients]);
+
+const filteredIngredientsHandler = useCallback(filteredIngredients => {
+  setUserIngredients(filteredIngredients);
+}, [setUserIngredients])
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-9b340-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json'}
+    }).then (response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients, 
+        { id: responseData.name, ...ingredient }
+      ]);
+    });  
+  ;}
+
+  const removeIngredientHandler = ingredientId => {
+    fetch(
+      `https://react-hooks-9b340-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, 
+    {
+      method: 'DELETE'
+    }
+    ).then(response => {
+      setUserIngredients(prevIngredients => 
+        prevIngredients.filter(ingredient => ingredient.id!== ingredientId)
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
+
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
