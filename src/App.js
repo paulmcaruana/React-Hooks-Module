97ad18248f8a3f17d0d402cb4c9768796bf09091
Paul Caruana <paul.caruana@leighton.com>
2020-12-22 15:16:39 +0000

    custom hooks added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useContext } from 'react';
 import Ingredients from './components/Ingredients/Ingredients';
 import Auth from './components/Auth';
 import { AuthContext } from './context/auth-context';
-import IngredientList from './components/Ingredients/IngredientList';
 
 const App = props => {
   const authContext = useContext(AuthContext);
